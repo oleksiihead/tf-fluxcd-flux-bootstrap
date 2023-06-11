ba89217004d73fa88f9c1e64a8221fa4977578e1
Oleksii Smiichuk <smiychuk@gmail.com>
2023-06-11 07:25:51 +0000

    Changed auth method

--- provider.tf
+++ provider.tf
@@ -9,7 +9,9 @@ terraform {
 
 provider "flux" {
   kubernetes = {
-    config_path = var.config_path
+    host                   = var.config_host
+    token                  = var.config_token
+    cluster_ca_certificate = var.config_ca
   }
   git = {
     url = "https://github.com/${var.github_repository}.git"
